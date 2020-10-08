b6932489cc89f2097ad37fca353d372b22a30462
till-tietz <63319462+till-tietz@users.noreply.github.com>
2020-10-08 14:40:50 +0200

    Update overlay.relief.R

--- R/overlay.relief.R
+++ R/overlay.relief.R
@@ -24,6 +24,7 @@ overlay.relief <- function(map.data,
                            altitude = 45,
                            azimuth = 270,
                            z.factor = NULL){
+  future::plan(multiprocess)
   if(missing(map.data)){stop("missing map.data")
   } else {
     if(class(map.data)[[1]] != "sf"){stop("map.data not of class sf")
@@ -58,7 +59,7 @@ overlay.relief <- function(map.data,
     relief_cropped[variable] <- rep(crop_area[,variable], nrow(relief_cropped))
     return(relief_cropped)
   }
-  relief_combined <- purrr::map(1:nrow(map.data), ~relief_assign(.x))
+  relief_combined <- furrr::future_map(1:nrow(map.data), ~relief_assign(.x))
   relief_out <- dplyr::bind_rows(relief_combined, .id = "column_label")%>%
     dplyr::select(-c(column_label))
   return(relief_out)
