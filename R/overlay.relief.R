5afa22a8d9fb836d9c7a94fa3330c6fd14fd122a
till-tietz <ttietz2014@gmail.com>
2020-12-01 12:09:42 +0100

    added functionality to handle multiple sf attribute variables

--- R/overlay.relief.R
+++ R/overlay.relief.R
@@ -8,60 +8,77 @@
 #'
 #'
 #' @param map.data An sf data frame with geometries of type MULTILINESTRING, POLYGON or MULTIPOLYGON and associated attributes.
-#' @param variable The column name of the attribute you wish to visualize on top of the relief passed to the argument as a character string i.e "variable".
+#' @param variable The column names of the attributes you wish to visualize on top of the relief passed to the argument as a character vector i.e c("variable_1","variable_2").
 #' @param elevation.raster Digital Elevation Model data in form of an object of class RasterLayer.
-#' @param coordinate.system Four digit epsg code of the projection you wish to use. Defaults to 4326.
+#' @param coordinate.system Four digit epsg code of the projection you wish to use. Default 4326.
 #' @param altitude Elevation angle of light source in degrees to calculate hill shade. Numeric value between 0 and 90.
 #' @param azimuth Direction angle of light source in degrees to calculate hill shade. Numeric value between 0 and 360.
-#' @param z.factor Numeric value to multiply elevation raster by to exaggerate relief. Defaults to NULL.
-#' @return A data.frame of points and their associated hill shade values and sf attributes.
+#' @param z.factor Numeric value to multiply elevation raster by to exaggerate relief. Default NULL.
+#' @return A data frame of points and their associated hill shade values and sf attributes.
 #' @export
 
 overlay.relief <- function(map.data,
-                           variable,
+                           variables,
                            elevation.raster,
                            coordinate.system = 4326,
                            altitude = 45,
                            azimuth = 270,
                            z.factor = NULL){
-  if(missing(map.data)){stop("missing map.data")
+  if(missing(map.data)) {
+    stop("missing map.data")
   } else {
-    if(class(map.data)[[1]] != "sf"){stop("map.data not of class sf")
-    } else {map_data <- map.data %>% sf::st_transform(., coordinate.system)
+    if (class(map.data)[[1]] != "sf") {
+      stop("map.data not of class sf")
+    } else {
+      map_data <- map.data %>% sf::st_transform(., coordinate.system)
     }
   }
-  if(missing(variable)){stop("Missing variable. Please specify the name of the variable in map.data you'd like to visualize")
+
+  if(missing(variable)) {
+    stop("Missing variable. Please specify the name of the variable in map.data you'd like to visualize")
   } else {
-      if(class(variable) != "character"){stop("Input for Variable is not of class character. Please input variable as character string")}
-    }
-  if(missing(elevation.raster)){stop("missing elevation.raster")
-  } else {
-    if(class(elevation.raster)[[1]] != "RasterLayer"){stop("elevation.raster not of class RasterLayer")
-    } else { elevation_raster <- elevation.raster %>% raster::projectRaster(., crs = raster::crs(map_data))
+    if (class(variable) != "character") {
+      stop("Input for Variable is not of class character. Please input variable as character string")
     }
   }
-  if(!is.null(z.factor)){raster <- (elevation_raster * z.factor)
+
+
+  if(missing(elevation.raster)) {
+    stop("missing elevation.raster")
+  } else {
+    if (class(elevation.raster)[[1]] != "RasterLayer") {
+      stop("elevation.raster not of class RasterLayer")
+    } else {
+      elevation_raster <- elevation.raster%>%
+        raster::projectRaster(., crs = raster::crs(map_data))
+    }
+  }
+
+  if(!is.null(z.factor)) {
+    raster <- (elevation_raster * z.factor)
   } else {
     raster <- elevation_raster
   }
+
   slope <- raster::terrain(raster, opt= "slope")
   aspect <- raster::terrain(raster, opt= "aspect")
   relief <- raster::hillShade(slope, aspect, angle= altitude, direction= azimuth)
 
   relief_assign <- function(x){
     crop_area <- map_data[x,]
+
     relief_cropped <- raster::crop(relief, extent(crop_area))%>%
       raster::mask(., crop_area)%>%
       raster::rasterToPoints()%>%
       data.frame()
+
     sf::st_geometry(crop_area) <- NULL
-    relief_cropped[variable] <- rep(crop_area[,variable], nrow(relief_cropped))
+    vars <- as.data.frame(sapply(crop_area[,variables], function(x) rep(x, nrow(relief_cropped))))
+    relief_cropped <- cbind(vars, relief_cropped)
     return(relief_cropped)
   }
-  relief_combined <- purrr::map(1:nrow(map.data), ~relief_assign(.x))
-  relief_out <- dplyr::bind_rows(relief_combined, .id = "column_label")%>%
-    dplyr::select(-c(column_label))
-  return(relief_out)
+  relief_combined <- purrr::map_dfr(1:nrow(map.data), ~relief_assign(.x))
+  return(relief_combined)
 }
 
 
