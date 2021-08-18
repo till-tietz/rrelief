19fdcec48b570ca38aac6c1915f8fb860ba20ea5
till-tietz <ttietz2014@gmail.com>
2021-08-18 15:09:22 +0200

    added utility function to generate DEM

---
+++ R/generate.dem.R
@@ -0,0 +1,68 @@
+#' turn elevation raster .tif files into a DEM
+#'
+#' generate.dem converts elevation raster .tif files into a DEM ready to use with overlay.relief
+#'
+#' @name generate.dem
+#'
+#' @param raster.files A vector of file paths to the elevation raster .tif files you wish to turn into a DEM.
+#' @param map.data An sf data frame with geometries of type POLYGON or MULTIPOLYGON and associated attributes.
+#' @param coordinate.system EPSG code of the projection you wish to use. Default NULL: if crs of map.data and the generated DEM do not match the latter will be projected to the crs of the former.
+#' @param aggregate factor to aggregate the generated DEM by. Default NULL.
+#' @return A raster object.
+#' @export
+
+
+generate.dem <- function(raster.files, map.data, coordinate.system = NULL, aggregate = NULL){
+
+  if (missing(raster.files)) {
+    stop("missing raster.list")
+  }
+
+  if (missing(map.data)) {
+    stop("missing map.data")
+  } else {
+    if (class(map.data)[[1]] != "sf") {
+      stop("map.data not of class sf")
+    }
+  }
+
+  print("loading raster files")
+  raster.files <- sapply(raster.files, raster::raster)
+  names(raster.files) <- NULL
+  raster.files$fun <- mean
+  print("raster files loaded")
+  print("combining raster files")
+  elevation.raster <- do.call(raster::mosaic, raster.files)
+  print("raster files combined")
+
+  if(is.na(raster::crs(elevation.raster))){
+    raster::crs(elevation.raster) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+  }
+
+  if(!is.null(aggregate)){
+    print("aggregating raster")
+    elevation.raster <- raster::aggregate(elevation.raster, aggregate)
+    print("raster aggregated")
+  }
+
+  if (is.null(coordinate.system)){
+    same_crs <- raster::compareCRS(map.data, elevation.raster)
+    if(!same_crs){
+      print("re-projecting elevation.raster")
+      elevation.raster <- raster::projectRaster(elevation.raster, crs = raster::crs(map.data))
+      print("elevation.raster projected")
+    }
+  } else {
+    print("re-projecting map.data")
+    map.data <- sf::st_transform(map.data, crs = coordinate.system)
+    print("map.data projected")
+    print("re-projecting elevation.raster")
+    elevation.raster <- raster::projectRaster(elevation.raster, crs = raster::crs(map.data))
+    print("elevation.raster projected")
+  }
+
+  elevation.raster <- raster::crop(elevation.raster, map.data)
+  elevation.raster <- raster::mask(elevation.raster, map.data)
+
+  return(elevation.raster)
+}
